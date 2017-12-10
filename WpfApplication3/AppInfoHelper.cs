15685969278abe635fae04b7210abb25586d8cf3
Chenchen <chenchen@beisen.com>
2017-12-11 00:00:54 +0800

    Issue：深度数据出现空白处

---
+++ WpfApplication3/AppInfoHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication3
+{
+    public class AppInfoHelper
+    {
+        static int currentSecond { get; set; }
+        static int callsPerSecond = 0;
+        static int lastResult = 0;
+        public static int LoopCallToGetCallsPerSecond()
+        {
+            if (currentSecond == DateTime.Now.Second)
+            {
+                callsPerSecond++;
+            }
+            else
+            {
+                lastResult = callsPerSecond;
+                callsPerSecond = 0;
+                currentSecond = DateTime.Now.Second;
+            }
+            return lastResult;
+        }
+    }
+}
--- WpfApplication3/MainWindow.xaml.cs
+++ WpfApplication3/MainWindow.xaml.cs
@@ -57,7 +57,7 @@ namespace WpfApplication3
         {
             KinectSensor.KinectSensors.StatusChanged += KinectSensors_StatusChanged;
             //程序启动时，如果Kinect未连接，这里this.Kinect是空
-            this.Kinect = KinectSensor.KinectSensors.FirstOrDefault(x => x.Status == KinectStatus.Connected); 
+            this.Kinect = KinectSensor.KinectSensors.FirstOrDefault(x => x.Status == KinectStatus.Connected);
         }
         private void KinectSensors_StatusChanged(object sender, StatusChangedEventArgs e)
         {
@@ -94,8 +94,8 @@ namespace WpfApplication3
         //----性能优化点1：使用WriteableBitmap，避免重复创建的开销----
         private WriteableBitmap writableBitMapDepthImage;
         private Int32Rect depthImageBitmapRect;
-        private Int32Rect depthImageNormalizeBitmapRect;
         private int depthImageStride; //
+        private ImageMergeProvider imageMerge;
         //------------------------------------------------------------
         private void InitializeKinectSensor(KinectSensor kinectSensor)
         {//此方法只会在KinectReady的时候被调用一次，用于初始化
@@ -113,32 +113,52 @@ namespace WpfApplication3
                 ColorImageElement.Source = writableBitMapDepthImage;
                 kinectSensor.DepthFrameReady += kinectSensor_DepthFrameReady; //每一帧调用一次
                 kinectSensor.Start();
+                imageMerge = new ImageMergeProvider(depthStream.FrameWidth * depthStream.FrameHeight,10);
             }
         }
         void kinectSensor_DepthFrameReady(object sender, DepthImageFrameReadyEventArgs e)
         {
             using (DepthImageFrame depthFrame = e.OpenDepthImageFrame())
             {
+                LabelPixelData.Content = AppInfoHelper.LoopCallToGetCallsPerSecond();
                 if (depthFrame != null)
                 {
                     short[] depthPixelDatas = new short[depthFrame.PixelDataLength];
                     depthFrame.CopyPixelDataTo(depthPixelDatas);
-                    
+
                     writableBitMapDepthImage.WritePixels(depthImageBitmapRect, depthPixelDatas, depthImageStride, 0);
+                    
+                    if (catchFlag)
+                    {
+                        var result = imageMerge.Merge(depthPixelDatas);
+                        if(result!=null)
+                        {
+                            writableBitMapDepthImage.WritePixels(depthImageBitmapRect, result, depthImageStride, 0);
+                            catchFlag = false;
+                        }
+                        SaveImageFile();
+                    }
                 }
+
             }
         }
 
-        
-
+        bool catchFlag = false;
+        private void MergePictureButton_Click(object sender, RoutedEventArgs e)
+        {
+            catchFlag = true;
+        }
         private void TakePictureButton_Click(object sender, RoutedEventArgs e)
         {
-            String fileName = DateTime.Now.ToString("yyyyMMddHHmmss")+"snapshot.jpg";
+            SaveImageFile();
+        }
+        private void SaveImageFile()
+        {
+            String fileName = DateTime.Now.ToString("yyyyMMddHHmmssfff") + "snapshot.jpg";
             if (File.Exists(fileName))
             {
                 File.Delete(fileName);
             }
-
             using (FileStream savedSnapshot = new FileStream(fileName, FileMode.CreateNew))
             {
                 BitmapSource image = (BitmapSource)ColorImageElement.Source;
@@ -164,5 +184,59 @@ namespace WpfApplication3
         }
     }
 
+    public static class Extensions
+    {
+        public static int GetRealDistance(this short source)
+        {
+            return source >> DepthImageFrame.PlayerIndexBitmaskWidth;
+        }
+    }
+
+    public class ImageMergeProvider
+    {
+        public short[] MergedResult { get; private set; }
+        int Times { get; set; }
+        public ImageMergeProvider(int length, int times)
+        {
+            MergedResult = Enumerable.Repeat<short>(-1, length).ToArray();
+            Times = times;
+        }
+        int currentTime = 0;
+        public short[] Merge(short[] source)
+        {
+            currentTime++;
+            if(currentTime<Times)  //如果当前次数小于规定次数
+            {
+                MergeToResult(source);
+                return null;
+            }
+            else
+            {
+                //输出图像
+                return MergedResult;
+            }
+        }
+        private short[] MergeToResult(short[] source)
+        {
+            int max = Math.Max(source.Length, MergedResult.Length);
+            for(int i=0;i<max;i++)
+            {
+                if (source[i] > 0)
+                {
+                    if (MergedResult[i] > 0)
+                    {
+                        //均值
+                        MergedResult[i] = (short)((source[i] + MergedResult[i]) / 2);
+                    }
+                    else
+                    {
+                        MergedResult[i] = source[i];
+                    }
+                }
+            }
+            return MergedResult;
+        }
+    }
+    
 
 }
